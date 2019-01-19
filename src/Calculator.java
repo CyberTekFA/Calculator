f67966738a5c5d211041ac41a6a385bb874d9bb7
CyberTekFA <46758994+CyberTekFA@users.noreply.github.com>
2019-01-19 16:35:25 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,5 +13,8 @@ public class Calculator {
     public static void minis(int num1,int num2){
         System.out.println(num1 - num2);
     }
+     public static void multi(int num1,int num2){
+        System.out.println(num1 * num2);
+    }
   
 }
