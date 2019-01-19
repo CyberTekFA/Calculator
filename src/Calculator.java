edcc28a7026154b927ce5b454c9715741c030975
CyberTekFA <faiz.m.amiree@gmail.com>
2019-01-19 12:39:10 -0500

    minis method added

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,4 +10,7 @@ public class Calculator {
     public static void addition(int num1 , int num2){
         System.out.println(num1 + num2);
     }
+    public static void minis(int num1,int num2){
+        System.out.println(num1 - num2);
+    }
 }
