21ec1a9ecbb1536296ddd8bcd9fbd54e9ac383a4
CyberTekFA <faiz.m.amiree@gmail.com>
2019-01-19 11:54:18 -0500

    Calculator Project

---
+++ src/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <11 ; i++) {
+            System.out.printf("%d x %d = %d ",5,i,i*5).println();
+        }
+    }
+}
