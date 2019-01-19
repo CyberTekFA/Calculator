512dc79c970b529b7e78a979f2c7b932e43ead0f
CyberTekFA <faiz.m.amiree@gmail.com>
2019-01-19 16:38:24 -0500

    Updated

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,5 +13,8 @@ public class Calculator {
     public static void minis(int num1,int num2){
         System.out.println(num1 - num2);
     }
+    public static void mulit(int num1,int num2){
+        System.out.println(num1 * num2);
+    }
   
 }
